05ac56622c0f82d3bcd9465d121cd87a6b07adbc
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-08 03:43:31 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@161e81f8861dacd1f9817a09f6d3cdfbd0552a75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-projects",
+          title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
