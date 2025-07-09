7341e25672634a42cca62c3f4a8f552a38ed2f4c
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-09 19:18:36 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@32ba052e6bf43ba8d374d1e0bb93891f0a0ffe10 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,28 +41,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-code-diff",
-        
-          title: "a post with code diff",
-        
-        description: "this is how you can display code diffs",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/code-diff/";
-          
-        },
-      },{id: "post-a-post-with-advanced-image-components",
-        
-          title: "a post with advanced image components",
-        
-        description: "this is what advanced image components could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/advanced-images/";
-          
-        },
       },{id: "post-a-post-with-geojson",
         
           title: "a post with geojson",
