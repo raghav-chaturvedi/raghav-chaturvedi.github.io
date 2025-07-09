a9221efd2b1b6ec2206b03b077df2dad6db4e6bb
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-09 14:49:45 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@b7478841c2b2481c92ec787320520bc91785c7bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,28 +459,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%61%67%68%61%76%63%68%61%74%75%72%76%65%64%69%30%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://instagram.com/raghav_ch", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=5Wv2hAQAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
