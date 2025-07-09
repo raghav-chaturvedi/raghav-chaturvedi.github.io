f95a435af1a8e3f420c7f2860a8a40c6e7e7abad
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-09 14:50:32 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@d2c3aa5530406a46e290c71cf772d4ecbe183906 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -480,7 +480,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://com.manychat.com", "_blank");
         },
       },{
       id: 'light-theme',
