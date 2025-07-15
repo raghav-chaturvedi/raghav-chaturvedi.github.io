676b96d8ebfc4ae728558fd58703ec4b45fc6af6
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-15 02:45:45 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@65f9d51cf0dcf7c992cdb50e66554b05d842e246 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,6 +70,17 @@ ninja.data = [{
             window.location.href = "/blog/2015/disqus-comments/";
           
         },
+      },{id: "post-a-post-with-math",
+        
+          title: "a post with math",
+        
+        description: "an example of a blog post with some math",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/math/";
+          
+        },
       },{id: "post-a-post-with-code",
         
           title: "a post with code",
