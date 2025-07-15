8c0f474af8c46a809724c5f6415cb2bee30bd530
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-15 03:09:51 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@1e6dbb07732e1fb5abaa50eb1bbfc1024d41ab8e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/courses/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+        },{id: "post-direct-preference-optimzation-with-a-swimmer-robot",
+        
+          title: "Direct preference optimzation with a swimmer robot",
+        
+        description: "an implementation of direct preference optimization (DPO)",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/code/";
+          
+        },
+      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
@@ -57,17 +68,6 @@ ninja.data = [{
         handler: () => {
           
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/code/";
           
         },
       },{id: "books-the-godfather",
