a5f72ac54333ac5acd413522d6c08493107edf6b
raghav-chaturvedi <raghavchaturvedi04@gmail.com>
2025-07-16 22:19:05 +0000

    Deploying to gh-pages from @ raghav-chaturvedi/raghav-chaturvedi.github.io@d25de3b97a523ae8fd50ad4a048778521c7f956d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -153,13 +153,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=5Wv2hAQAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
